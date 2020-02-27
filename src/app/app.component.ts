6330e643eb0580cd54e7b277054686122bfe545d
jose00796 <josedarkrai@gmail.com>
2020-02-27 00:23:50 -0400

    Agregamos un metodo EsMuyListo y hacemos la llamada a traves de un NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Migdaly es Preciosa';
   show:boolean = true;
+  materia:string = 'programacion';
   calc:number = 0;
+
+  EsMuyListo()
+  {
+    if(this.calc > 9 && this.materia == 'programacion'){
+        return true;
+    }else{
+      return false;
+    }
+  }
 }
