7850db2045d62c7bc7c9c3cad1aa7bf1b4eb89fa
jose00796 <josedarkrai@gmail.com>
2020-03-04 01:04:53 -0400

    Practica NgFor

--- src/app/directiva-ng/directiva-ng.component.ts
+++ src/app/directiva-ng/directiva-ng.component.ts
@@ -12,6 +12,17 @@ export class DirectivaNGComponent implements OnInit {
   calc:number = 0;
   activo:string;
 
+  MisGustos:string[] = ['Mujeres', 'Dulces', 'Cafe', 'Futbol', 'Anime', 'Musica'];
+
+  personas:Array<any> = [
+    {nombre:'Jose', apellido:'Machado', cedula:25418204, edo_civil:'Soltero'},
+    {nombre:'Migdaly', apellido:'Tarazona', cedula:27420310, edo_civil:'Soltero'},
+    {nombre:'Franmy', apellido:'Tarazona', cedula:27420309, edo_civil:'Soltero'},
+    {nombre:'Pedro', apellido:'Rosal', cedula:25890567, edo_civil:'Soltero'},
+    {nombre:'Esmeralda', apellido:'Fuentes', cedula:6102995, edo_civil:'Casado'},
+  ];
+
+
   EsMuyListo()
   {
     if(this.calc > 9 && this.materia == 'programacion'){
