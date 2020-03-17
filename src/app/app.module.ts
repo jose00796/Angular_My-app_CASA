864523558dc5bcdc3b83eb8a15c3bbc686d3e152
jose00796 <josedarkrai@gmail.com>
2020-03-16 23:38:33 -0400

    Creamos un componente COntacto y le agregamos un Formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DirectivaNGComponent } from './directiva-ng/directiva-ng.component';
 import { HorarioComponent } from './horario/horario.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const routes: Routes = [
+  { path: 'horario', component: HorarioComponent },
+  { path: 'directiva', component: DirectivaNGComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +21,12 @@ import { FooterComponent } from './footer/footer.component';
     DirectivaNGComponent,
     HorarioComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
